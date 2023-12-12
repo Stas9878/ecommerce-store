3dd5c12c2a6346fd485f24000f62ebd4d37cb15f
Popov Stas <boss.staspopov@mail.ru>
2023-12-12 18:51:01 +0300

    Добавил логики и html

--- ecom_store/core/views.py
+++ ecom_store/core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from product.models import Product
 
-def index(request):
-    return HttpResponse('Welcome')
+def frontpage(request):
+    products = Product.objects.all()[:8]
+    return render(request, 'core/frontpage.html', {'products': products})
+
+def shop(request):
+    products = Product.objects.all()
+    return render(request, 'core/shop.html', {'products': products})
